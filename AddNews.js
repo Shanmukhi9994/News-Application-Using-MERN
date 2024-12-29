4abb664f0680811e0a7cc8012523376b53e30c65
ShanmukhiBoddu <151181980+Shanmukhi9994@users.noreply.github.com>
2024-12-29 17:52:07 +0530

    Add files via upload

---
+++ AddNews.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AddNewsForm from '../components/AddNewsForm';
+
+const AddNews = () => {
+  return (
+    <div>
+      <h2>Add News</h2>
+      <AddNewsForm />
+    </div>
+  );
+};
+
+export default AddNews;
